05efa730541fcaf26a88ac60b1d038ea07a9554a
Byron Theobald <byrontheobald@gmail.com>
2023-06-10 16:26:12 +0100

    added back st7735

---
+++ driver/display/st7735/include/st7735.h
@@ -0,0 +1,80 @@
+#ifndef __ST7735_H__
+#define __ST7735_H__
+
+#include "hardware/spi.h"
+#include "hardware/gpio.h"
+
+// Success / Error
+// -----------------------------------
+#define ST7735_SUCCESS        0
+#define ST7735_ERROR          1
+
+// Command definition
+// -----------------------------------
+#define DELAY                 0x80
+
+#define NOP                   0x00
+#define SWRESET               0x01
+#define RDDID                 0x04
+#define RDDST                 0x09
+
+#define SLPIN                 0x10
+#define SLPOUT                0x11
+#define PTLON                 0x12
+#define NORON                 0x13
+
+#define INVOFF                0x20
+#define INVON                 0x21
+#define DISPOFF               0x28
+#define DISPON                0x29
+#define RAMRD                 0x2E
+#define CASET                 0x2A
+#define RASET                 0x2B
+#define RAMWR                 0x2C
+
+#define PTLAR                 0x30
+#define MADCTL                0x36
+#define COLMOD                0x3A
+
+#define FRMCTR1               0xB1
+#define FRMCTR2               0xB2
+#define FRMCTR3               0xB3
+#define INVCTR                0xB4
+#define DISSET5               0xB6
+
+#define PWCTR1                0xC0
+#define PWCTR2                0xC1
+#define PWCTR3                0xC2
+#define PWCTR4                0xC3
+#define PWCTR5                0xC4
+#define VMCTR1                0xC5
+
+#define RDID1                 0xDA
+#define RDID2                 0xDB
+#define RDID3                 0xDC
+#define RDID4                 0xDD
+
+#define GMCTRP1               0xE0
+#define GMCTRN1               0xE1
+
+#define PWCTR6                0xFC
+
+// Colors
+// -----------------------------------
+#define BLACK                 0x0000
+#define WHITE                 0xFFFF
+#define RED                   0xF000
+
+typedef struct {
+    spi_inst_t *spi_ref;  // SPI Reference Pointer
+    uint8_t dc_gpio;
+    uint8_t reset_gpio;
+    uint8_t backlight_gpio;
+} st7735_t;
+
+void st7735_init (st7735_t * lcd, spi_inst_t * spi, uint8_t dc, uint8_t xres, uint8_t yres);
+void st7735_cmd_send (st7735_t * lcd, uint8_t * cmd);
+void st7735_data_send (st7735_t * lcd, uint8_t * data, uint8_t len);
+void st7735_setup_fill(st7735_t * lcd);
+
+#endif
---
+++ driver/display/st7735/source/st7735.c
@@ -0,0 +1,78 @@
+#include "st7735.h"
+#include "pico/stdlib.h"
+
+/** @array Init command */
+const uint8_t st7735B_Init[] = {
+    SWRESET,  0, 
+    SLPOUT,   0,
+    COLMOD,   1,  0x05,
+    FRMCTR1,  3,  0x00, 0x06, 0x03,
+    MADCTL,   1,  0xC0,                     // 0xC0 - 0xE0
+    DISSET5,  2,  0x15, 0x02,
+    INVCTR,   1,  0x0,
+    PWCTR1,   2,  0x02, 0x70,
+    PWCTR2,   1,  0x05,
+    //PWCTR3,   2,  0x01, 0x02,
+    //VMCTR1,   2,  0x3C, 0x38,
+    //PWCTR6,   2,  0x11, 0x15,*/
+    GMCTRP1,  16,
+      0x09, 0x16, 0x09, 0x20,
+      0x21, 0x1B, 0x13, 0x19,
+      0x17, 0x15, 0x1E, 0x2B,
+      0x04, 0x05, 0x02, 0x0E,
+    GMCTRN1,  16,
+      0x0B, 0x14, 0x08, 0x1E,
+      0x22, 0x1D, 0x18, 0x1E,
+      0x1B, 0x1A, 0x24, 0x2B,
+      0x06, 0x06, 0x02, 0x0F,
+    NORON,    0,
+    DISPON,   0
+};
+
+uint8_t st7735B_Fill[] = {
+    CASET,  4,  0, 1, 0, 130,  
+    RASET,  4,  0, 1, 0, 160,
+    RAMWR,  0
+};
+
+void exec_cmd_array(st7735_t * lcd, const uint8_t * cmdset, uint8_t size, uint8_t us_sleep) {
+    for(int cmd = 0; cmd < size; cmd+=2) {
+        st7735_cmd_send(lcd, (uint8_t *)&(cmdset[cmd]));
+        if(cmdset[cmd+1] > 0) {
+            st7735_data_send(lcd, (uint8_t *)&cmdset[cmd+2], cmdset[cmd+1]);  
+            cmd+=cmdset[cmd+1];
+        }
+        if(us_sleep > 0) sleep_us(us_sleep);
+    }
+}
+
+void st7735_init (st7735_t * lcd, spi_inst_t * spi, uint8_t dc, uint8_t xres, uint8_t yres) {
+    lcd->spi_ref = spi;
+    lcd->dc_gpio = dc;
+
+    gpio_init(lcd->dc_gpio);
+    gpio_set_dir(lcd->dc_gpio, 1);
+    gpio_put(lcd->dc_gpio, 0); // Default - Command
+
+    //st7735B_Fill[5] = yres;
+    //st7735B_Fill[11] = xres;
+
+    exec_cmd_array(lcd, st7735B_Init, sizeof(st7735B_Init), 100);
+}
+
+void st7735_cmd_send (st7735_t * lcd, uint8_t * cmd) {
+    gpio_put(lcd->dc_gpio, 0);
+    spi_write_blocking(lcd->spi_ref, cmd, 1);
+}
+
+void st7735_data_send (st7735_t * lcd, uint8_t * data, uint8_t len) {
+    gpio_put(lcd->dc_gpio, 1);
+    spi_write_blocking(lcd->spi_ref, data, len);
+}
+
+void st7735_setup_fill(st7735_t * lcd) {
+    spi_set_format(lcd->spi_ref, 8, SPI_CPOL_1, SPI_CPHA_1, SPI_MSB_FIRST); // SPI MODE 3 - 5.36ms per Frame
+    exec_cmd_array(lcd, st7735B_Fill, sizeof(st7735B_Fill), 0);
+    gpio_put(lcd->dc_gpio, 1); // Data burst will follow
+    spi_set_format(lcd->spi_ref, 16, SPI_CPOL_1, SPI_CPHA_1, SPI_MSB_FIRST); // SPI MODE 3 - 5.36ms per Frame
+}
